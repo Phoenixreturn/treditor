65a101667712756b30ec2e0d8b4a4bd613f0775d
Phoenix <rob.gallyamov@gmail.com>
2020-02-27 18:17:40 +0500

    Add stub resizable component

---
+++ src/components/Resizable.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="resizable">
+        Resizable component
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Resizable",    
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import HelloWorld from './components/HelloWorld.vue'
+import Resizable from './components/Resizable.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: HelloWorld },
+  { path: '/resizable', component: Resizable }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
